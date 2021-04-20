3fe7879b8b62f88a47087bb639e56a8d1352a4fd
pesdesigner <pauloeduardo.dg@outlook.com>
2021-04-20 20:36:07 -0300

    Project created

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import {Text, Image, StyleSheet, SafeAreaView}  from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+import { Button } from '../components/Button';
+import colors from '../styles/colors';
+
+export function Welcome(){
+  const [visible, setVisible] = useState(false); 
+
+  function handleVisibility(){
+    setVisible(true)
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas {'\n'}
+        de forma fácil
+      </Text>
+    
+        <Image source={wateringImg} style={styles.image} />
+    
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas.
+        Nós cuidamos de lembrar você sempre que precisar.
+      </Text>
+
+      <Button title='+'/>  
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    subtitle: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: colors.heading,
+        paddingHorizontal: 20
+    },
+    image: {
+        width: 292,
+        height: 284
+    }
+})
